9b696cda916e289b8bb0286d7e512ed94ebab656
krina <krinabhalani@gmail.com>
2024-11-21 21:57:48 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+
+<head>
+    <!-- Meta Data -->
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>My healing-hands</title>
+    <meta name="description" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <link rel="stylesheet" href="assets/css/vendor/bootstrap.min.css" />
+    <link rel="stylesheet" href="assets/css/app.css" />
+</head>
+
+<body class="sticky-header">
+
+    <section id="main-wrapper" class="section-exciting__news">
+        <div class="container m-section-my">
+            <div class="section-1">
+                <div class="main-aboutus_box">
+                    <div class="m-section__header m:text-left">
+                        <img src="assets/images/Logo.svg" class="h-logo">
+                        <h2 class="m-section__heading h2">Exciting <span>News!</span></h2>
+                        <div class="m-section__description">
+                            <p>Our website will soon be available here, bringing you all the information about our range
+                                of effective, clinically proven plant-based medicines for gastrointestinal and anorectal
+                                health. We're working hard to ensure a seamless experience for our users here.</p>
+                            <p>In the meantime, if you're curious to learn more about our products, please visit our
+                                site from India below.</p>
+                        </div>
+                    </div>
+                    <div class="aboutus-begin_icon">
+                        <img
+                            src="https://cdn.shopify.com/s/files/1/0770/7990/4564/files/let_the_healing_begin_green_opacity.svg?v=1707820712">
+                        <svg width="40" height="56" viewBox="0 0 40 56" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                                d="M10.3677 55.7666C10.1122 54.2635 9.94594 52.8154 9.61118 51.4081C8.77208 47.8755 6.71727 45.0102 4.46755 42.2683C3.73857 41.3774 3.03601 40.4634 2.32355 39.5616C-0.641941 35.8175 -0.658458 31.9039 1.64632 27.8406C3.59838 24.592 5.95096 21.6016 8.64875 18.9397C9.37553 18.1843 10.1585 17.4828 10.8764 16.7186C11.027 16.5285 11.2344 16.3914 11.4683 16.3272C11.7021 16.2631 11.9504 16.2752 12.1769 16.3618C12.4255 16.4322 12.6451 16.5802 12.8036 16.7843C12.9621 16.9884 13.0512 17.2377 13.0579 17.496C13.1329 19.0095 12.864 20.5203 12.2716 21.9151C11.1513 24.799 10.3593 27.7999 9.9107 30.8611C9.60516 32.999 9.78545 35.1783 10.4382 37.237C11.2894 39.7091 13.026 39.8446 14.964 38.6178C15.2146 38.3491 15.3522 37.9942 15.3483 37.6268C15.5185 35.2801 16.5278 33.0731 18.1916 31.4095C19.2552 30.219 20.6483 29.3712 22.1944 28.9737C22.5168 28.8982 22.8486 28.8715 23.1789 28.8944C24.2569 28.977 24.5884 29.6311 24.0411 30.5462C23.5595 31.2662 23.1184 32.0125 22.7197 32.7816C22.2971 33.7753 22.0834 34.8452 22.0918 35.925C22.1001 37.0047 22.3303 38.0713 22.7681 39.0583C23.191 40.1826 23.6634 41.2882 24.0686 42.4191C24.3444 43.1933 24.4574 44.0161 24.4006 44.836C24.3437 45.6559 24.1182 46.4552 23.7383 47.184C22.3271 50.0358 19.9301 52.2794 16.9913 53.4992C15.0499 54.3802 12.9786 54.9682 10.9612 55.6763C10.766 55.7279 10.566 55.7596 10.3644 55.771"
+                                fill="#02363A" />
+                            <path
+                                d="M29.3763 0.768066C29.732 2.53987 29.9644 4.29185 30.4467 5.97446C31.3783 9.21635 33.3945 11.8438 35.4758 14.4216C36.4669 15.6539 37.4502 16.9092 38.3234 18.2295C39.1629 19.4657 39.6706 20.8968 39.7981 22.3856C39.9256 23.8743 39.6684 25.3709 39.0513 26.7318C37.8138 29.579 36.0947 32.1914 33.9694 34.4544C32.3616 36.2735 30.6163 37.9694 28.948 39.7401C28.7986 39.9464 28.5829 40.0952 28.337 40.1618C28.0911 40.2283 27.8299 40.2085 27.5968 40.1056C27.3336 40.0302 27.106 39.8631 26.9553 39.6344C26.8047 39.4058 26.7408 39.1308 26.7753 38.8591C26.9732 37.399 27.25 35.9506 27.6045 34.5204C28.2597 32.0009 29.1187 29.531 29.6968 26.9972C30.2814 24.5685 30.218 22.0289 29.5129 19.6324C29.4339 19.3628 29.339 19.098 29.2288 18.8396C29.0645 18.4059 28.792 18.0214 28.4373 17.7225C28.0826 17.4237 27.6574 17.2205 27.202 17.1323C26.7467 17.044 26.2764 17.0736 25.8357 17.2182C25.395 17.3629 24.9987 17.6177 24.6842 17.9586C24.5337 18.187 24.4568 18.4561 24.4639 18.7295C24.3631 20.556 23.7536 22.3179 22.7042 23.8162C21.6548 25.3145 20.2073 26.4896 18.5253 27.2086C17.9422 27.4634 17.3115 27.5907 16.6753 27.5819C15.5741 27.5819 15.2437 26.8848 15.8119 25.9455C16.2807 25.2472 16.709 24.5225 17.0948 23.7751C17.5245 22.7579 17.7406 21.6632 17.7296 20.5591C17.7186 19.4549 17.4808 18.3647 17.031 17.3563C16.6213 16.2815 16.1764 15.22 15.7789 14.1419C15.4924 13.3582 15.3731 12.5233 15.4287 11.6907C15.4842 10.8581 15.7135 10.0464 16.1016 9.30774C17.3828 6.65101 19.563 4.53355 22.2561 3.33052C24.4144 2.31963 26.7247 1.6347 28.9733 0.815417C29.1025 0.781458 29.2368 0.771014 29.3697 0.784584"
+                                fill="#02363A" />
+                        </svg>
+                    </div>
+                </div>
+
+                <div class="aboutus-banner">
+                    <img src="assets/images/about-us-img.png" class="hide-on-mobile">
+                    <img src="assets/images/about_us_mobile_img1.webp" class="hide-on-desk">
+                </div>
+                <div class="h-button">
+                    <a href="https://myhealinghands.in/" class="h-btn" target="_blank">Healing Hands India Website</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="section-stay__update">
+        <div class="container m-section-my">
+            <div class="stay-content__box">
+                <div class="m-section__subheading">Stay Updated</div>
+                <div class="m-section__description">
+                    <p>Want to be the first to know when our site goes live? Enter your email below, and we'll keep you posted on our launch!</p>
+                </div>
+                <div class="stay-update__form">
+                    <form action="" method="POST">
+                        <div class="form-group">
+                            <input type="email" class="form-control" id="email" placeholder="Enter email" name="email" required>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+
+                    <!-- PHP Success/Error Message -->
+                    <?php
+                    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                        // Get the email address from the form
+                        $to_email = $_POST['email'];
+                        
+                        // Email details
+                        $subject = "Myhealinghands.au newsletter submission";
+                        $message = $to_email;
+                        $headers = "From: care@myhealinghands.in"; // Replace with your email address
+                        
+                        // Variable to hold the message to display
+                        $message_display = '';
+                        
+                        // Send the email
+                        if (mail($to_email, $subject, $message, $headers)) {
+                            // Success message
+                            $message_display = "<div class='alert alert-success'>Thank you! We’ll be in touch soon with updates and exciting news.</div>";
+                        } else {
+                            // Error message
+                            $message_display = "<div class='alert alert-danger'>Failed to send email. Please try again.</div>";
+                        }
+                        
+                        // Display the message inside the form section
+                        echo $message_display;
+                    }
+                    ?>
+                </div>
+                <img src="assets/images/form-img.png">
+            </div>
+        </div>
+    </section>
+
+    <script src="assets/js/vendor/bootstrap.min.js"></script>
+</body>
+
+</html>
